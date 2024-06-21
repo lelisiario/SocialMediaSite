8ff146321ea45b1b2cb23afb02d86a99da8ac756
Lauren Elisiario <lauren.elisiario@gmail.com>
2024-06-21 10:46:08 -0700

    Added code for server.js, connection.js and index.js.

---
+++ Server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialMediaDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
