ba7aa0dd197f76eee800ab23bbdc65a05a3c388e
Lauren Elisiario <lauren.elisiario@gmail.com>
2024-06-21 18:18:13 -0700

    Added code for thought.js, user.js, thoughtRoutes.js and userRoutes.js

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match a valid email address'],
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought',
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+}, {
+  toJSON: {
+    virtuals: true,
+  },
+  id: false,
+});
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
